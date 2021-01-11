a204f49cea60157b7802f244c5423397fc330529
Collided Scope <collidedscope+github@protonmail.com>
2021-01-11 16:12:36 -0500

    Add 2dup and 2swap builtins

--- lib/phelix/builtins/stack.cr
+++ lib/phelix/builtins/stack.cr
@@ -2,6 +2,14 @@ class Phelix
   # duplicate [a] => [a a]
   defb "dup" { s << s[-1].dup }
 
+  defb "2dup" {
+    2.times {s << s[-2].dup }
+  }
+
+  defb "2swap" {
+    s[-4], s[-3], s[-2], s[-1] = s[-2], s[-1], s[-4], s[-3]
+  }
+
   # remove [a] => []
   defb "pop" { s.pop }
 
