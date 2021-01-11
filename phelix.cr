f33500c8281539bb73b94b3e25190288e70bfcf9
Collided Scope <collidedscope+github@protonmail.com>
2021-01-11 00:24:45 -0500

    Add bare-bones REPL

--- phelix.cr
+++ phelix.cr
@@ -4,5 +4,13 @@ if file = ARGV.shift?
   abort "no such file: '#{file}'" unless File.exists? file
   Phelix.new(Phelix.tokenize File.read file).evaluate
 else
-  abort "usage: phelix FILE [ARGS]"
+  stack = [] of Phelix::Value
+  loop do
+    print "λ "
+    if expr = STDIN.gets
+      p Phelix.new(Phelix.tokenize expr).evaluate stack
+    else
+      break
+    end
+  end
 end
