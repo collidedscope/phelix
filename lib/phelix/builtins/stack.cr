f4a9da0f9690dde0f9875da6368f5d5d02edf931
Collided Scope <collidedscope+github@protonmail.com>
2021-01-11 01:12:52 -0500

    Add "splay" builtin for un-arraying

--- lib/phelix/builtins/stack.cr
+++ lib/phelix/builtins/stack.cr
@@ -10,4 +10,8 @@ class Phelix
 
   # rotate the stack such that [a b c] => [b c a]
   defb "rot" { s[-3], s[-2], s[-1] = s[-2], s[-1], s[-3] }
+
+  defb "\\" {
+    get(Array).each { |e| s << e }
+  }
 end
