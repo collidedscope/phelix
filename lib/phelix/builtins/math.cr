5ed857cb8571c5ec2d2b29d65de3dfe61225d804
Collided Scope <collidedscope+github@protonmail.com>
2021-01-30 00:35:48 -0500

    Utilize overloading to clean up generated builtins

--- lib/phelix/builtins/math.cr
+++ lib/phelix/builtins/math.cr
@@ -1,56 +1,35 @@
 class Phelix
+  CMP = [Num, Str]
+
   macro bin_op(op)
-    dbi {{op.id.stringify[0..0]}} do
-      case a = s.pop
-      when Vec
-        s << a.reduce { |m, n|
-          m.as(Num) {{op.id}} n.as(Num)
-        }
-      when Num
-        s << get(Num) {{op.id}} a
-      else
-        abort "can't #{{{op}}} your #{s}"
-      end
+    dbi {{op[0..0]}}, Num, Num do |a, b|
+      s << a {{op.id}} b
+    end
+
+    dbi {{op[0..0]}}, Vec do |v|
+      s << v.reduce { |a, b| a.as(Num) {{op.id}} b.as(Num) }
     end
   end
 
   macro chain_op(op)
-    dbi {{op.id.stringify[0..0]}} do
-      case a = s.pop
-      when Vec
-        s << a.each_cons(2).all? { |(m, n)|
-          m.as(Num) {{op.id}} n.as(Num)
-        }
-      when Num
-        s << (get(Num) {{op.id}} a)
-      when Str
-        s << (get(Str) {{op.id}} a)
-      else
-        abort "can't #{{{op}}} your #{s}"
+    {% for t in CMP %}
+      dbi {{op[0..0]}}, {{t}}, {{t}} do |a, b|
+        s << (a {{op.id}} b)
       end
+    {% end %}
+
+    dbi {{op[0..0]}}, Vec do |v|
+      s << v.each_cons(2).all? { |(a, b)| a.as(Num) {{op.id}} b.as(Num) }
     end
   end
 
-  # pops the top two values and pushes their sum unless the top value is a
-  # vector, in which case the result is the vector's reduction over addition
-  bin_op(:+)
-  # pops the top two values and pushes their difference unless the top value is a
-  # vector, in which case the result is the vector's reduction over subtraction
-  bin_op(:-)
-  # pops the top two values and pushes their product unless the top value is a
-  # vector, in which case the result is the vector's reduction over multiplication
-  bin_op(:*)
-  # pops the top two values and pushes their quotient unless the top value is a
-  # vector, in which case the result is the vector's reduction over division
-  bin_op(://)
-  # pops the top two values and pushes their modulus unless the top value is a
-  # vector, in which case the result is the vector's reduction over modulation
-  bin_op(:%)
+  bin_op("+")
+  bin_op("-")
+  bin_op("*")
+  bin_op("//")
+  bin_op("%")
 
-  # pops the top two values and pushes whether the first is less than the second
-  chain_op(:<)
-  # pops the top two values and pushes whether the first is greater than the second
-  chain_op(:>)
-  # pops the top two values and pushes whether they're equal
-  chain_op(:==)
+  chain_op("<")
+  chain_op(">")
+  chain_op("==")
 end
