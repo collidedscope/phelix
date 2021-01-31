8b2f69b6a4407e22094a8728792a3a47939b486d
Collided Scope <collidedscope+github@protonmail.com>
2021-01-31 00:14:33 -0500

    Optimize `digits` (convert to builtin)

--- lib/phelix/builtins/numbers.cr
+++ lib/phelix/builtins/numbers.cr
@@ -6,4 +6,12 @@ class Phelix
   dbi "odd?", Num do |n|
     s << n.odd?
   end
+
+  dbi "digits", Num do |n|
+    s << n.digits.map &.to_big_i.as Val
+  end
+
+  dbi "digits/b", Num, Num do |n, b|
+    s << n.digits(b).map &.to_big_i.as Val
+  end
 end
